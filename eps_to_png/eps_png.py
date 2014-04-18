dee03dcb47c9f3b5627de65d8c0721779788518b
leileibill <leileibill@gmail.com>
2014-04-18 14:12:02 -0500

    Wokring copy of eps to png converter

---
+++ eps_to_png/eps_png.py
@@ -0,0 +1,42 @@
+import sys
+import time
+import os
+import shutil
+import subprocess
+
+watchdir = '//ad.uillinois.edu/engr/ece-power/Projects/pilawa-group/eps_to_png/'
+contents = os.listdir(watchdir)
+count = len(contents)
+dirmtime = os.stat(watchdir).st_mtime
+
+print count
+print contents
+print dirmtime
+while True:
+    newmtime = os.stat(watchdir).st_mtime
+    if newmtime != dirmtime:
+        dirmtime = newmtime
+        newcontents = os.listdir(watchdir)
+        matches = [files for files in newcontents if '.eps' in files]
+        print matches
+        for files in matches:
+            fileName, fileExtension = os.path.splitext(files)
+##            print fileName
+##            print fileExtension
+            subprocess.call(['C:\\Program Files\ImageMagick-6.8.9-Q16\convert', '-density','300', watchdir + files,watchdir + fileName + '.png'])
+##            cmd = "C:\Program Files\ImageMagick-6.8.9-Q16\convert" +' -density 300 '+ '"'+ watchdir + files + '"' + ' ' + '"' + watchdir + fileName + '.png' + '"'
+##            print cmd
+####            os.system(cmd)
+            shutil.copy(watchdir+files,watchdir+'processed/'+files)
+            os.remove(watchdir+files)
+##        added = set(newcontents).difference(contents)
+##        if added:
+##            print "Files added: %s" %(" ".join(added))
+##        removed = set(contents).difference(newcontents)
+##        if removed:
+##            print "Files removed: %s" %(" ".join(removed))
+##
+##        contents = newcontents
+    time.sleep(5)
+##cmd = '"C:\Program Files\ImageMagick-6.8.9-Q16\convert" -density 300 sample.eps sample.png'
+##os.system(cmd)
