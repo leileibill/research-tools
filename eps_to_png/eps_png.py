c2c661c4636346365e651b66260068e64a7159a1
leileibill <leileibill@gmail.com>
2014-10-17 17:03:35 -0500

    Added ps support and some comments

--- eps_to_png/eps_png.py
+++ eps_to_png/eps_png.py
@@ -1,10 +1,14 @@
+# This script monitors a folder and converts any ps eps and pdf files
+# into png
+# It uses ImageMagick library
 import sys
 import time
 import os
 import shutil
 import subprocess
 
-watchdir = '//ad.uillinois.edu/engr/ece-power/Projects/pilawa-group/eps_to_png/'
+# Folder to monitor
+watchdir = '//engr-power-01/Power/Projects/pilawa-group/eps_to_png/'
 contents = os.listdir(watchdir)
 count = len(contents)
 dirmtime = os.stat(watchdir).st_mtime
@@ -12,12 +16,14 @@ dirmtime = os.stat(watchdir).st_mtime
 print count
 print contents
 print dirmtime
+# For each 5 second period, check files and dates to see if there is any new
+# files that have been added
 while True:
     newmtime = os.stat(watchdir).st_mtime
     if newmtime != dirmtime:
         dirmtime = newmtime
         newcontents = os.listdir(watchdir)
-        matches = [files for files in newcontents if ('.eps' in files) or ('.pdf' in files)]
+        matches = [files for files in newcontents if ('.ps' in files) or ('.eps' in files) or ('.pdf' in files)]
         print matches
         for files in matches:
             try:
@@ -26,7 +32,12 @@ while True:
                 shutil.copy(watchdir+files,watchdir+'processed/'+files)
                 os.remove(watchdir+files)
             except IOError:
-                f = open('logfile', 'w')
+                f = open('Error_log', 'w')
+                f.write('I/O error({0}): {1}'.format(e.errno, e.strerror))
+                f.close();
+                pass
+            except WindowsError:
+                f = open('Error_log', 'w')
                 f.write('I/O error({0}): {1}'.format(e.errno, e.strerror))
                 f.close();
                 pass
