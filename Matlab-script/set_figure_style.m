b15af259b2c10a64c3751a6e774367b99aa7a8c5
leileibill <leileibill@gmail.com>
2014-11-28 11:43:57 -0600

    Added minor grid

--- Matlab-script/set_figure_style.m
+++ Matlab-script/set_figure_style.m
@@ -1,5 +1,7 @@
 set(gca,'xcolor',[0.75 0.75 0.75],'ycolor',[0.75 0.75 0.75])
+set(gca,'GridLineStyle','-')
 set(gca,'MinorGridLineStyle','-')
+% set(gca,'MajorGridLineStyle','-')
 grid on
 c=copyobj(gca,gcf);
 set(c,'color','none','xcolor','k', ...
