63991c86077eb0384c1456a7e58abf21f1aa5525
leileibill <leileibill@gmail.com>
2014-12-19 07:04:59 +0800

    fixed bug

--- Matlab-script/resize_figure.m
+++ Matlab-script/resize_figure.m
@@ -1,9 +1,11 @@
 function resize_figure(width, height)
-
-if nargin < 2
-  width =   300
+if nargin <1
+    width=300;
 end
-height=width*0.4;
+if nargin < 2
+  height=width*0.4;
+end
+
 set(gcf, 'Position', [200 200 200+width 200+height])
 set(gcf,'PaperPositionMode','auto')
 end
