93b8c170312c3dc3ed193c4236dbd8f7a531504f
leileibill <leileibill@gmail.com>
2014-12-19 07:38:33 +0800

    Changed default
    width to 250, aspect ratio to 0.3

--- Matlab-script/resize_figure.m
+++ Matlab-script/resize_figure.m
@@ -1,11 +1,11 @@
 function resize_figure(width, height)
 
 if nargin <1
-    width=300;
+    width=250;
 end
 
 if nargin < 2
-  height=width*0.4;
+  height=width*0.3;
 end
 
 set(gcf, 'Position', [200 200 200+width 200+height])
