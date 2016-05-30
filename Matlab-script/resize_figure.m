3fc17fa88bb93186a52a25fb337cb85111caa4ac
leileibill <leileibill@gmail.com>
2016-05-30 14:21:51 -0500

    updated resize figure to make the default larger

--- Matlab-script/resize_figure.m
+++ Matlab-script/resize_figure.m
@@ -1,11 +1,11 @@
 function resize_figure(width, height)
 
 if nargin <1
-    width=250;
+    width=360;
 end
 
 if nargin < 2
-  height=width*0.3;
+  height=width*0.5;
 end
 
 set(gcf, 'Position', [200 200 200+width 200+height])
