448a7e4c55cc2dde2847647b4edf5d0d3045dd2b
leileibill <leileibill@gmail.com>
2016-07-08 03:14:44 -0500

    Major change
    Updated matlab plotting scripts to use latex interpreter. Added
    computer modern fonts

--- Matlab-script/resize_figure.m
+++ Matlab-script/resize_figure.m
@@ -1,14 +1,19 @@
 function resize_figure(width, height)
 
 if nargin <1
-    width=250;
+    width=7;
 end
 
 if nargin < 2
-  height=width*0.3;
+  height=width*0.6;
 end
 
-set(gcf, 'Position', [200 200 200+width 200+height])
+% f = gcf;
+% f.Units = 'inches';   % works only after 2014b
+
+po = 1;
+set(gcf, 'Units', 'inches')
+set(gcf, 'Position', [po po width height])
 set(gcf,'PaperPositionMode','auto')
 
 end
