f4cc8325a1336e7a6e2a9b561d83017947e28932
leileibill <leileibill@gmail.com>
2016-08-04 21:15:43 -0500

    More flexible

--- Matlab-script/export_figure.m
+++ Matlab-script/export_figure.m
@@ -1,12 +1,27 @@
-function export_figure(name)
+function export_figure(name,options)
 
-if nargin <1
-    name =  'matlab_figure';
-end
+    if nargin <1
+        name =  'matlab_figure';
+    end
 
-savefig(name);  % same as .fig
-print('-dpng','-r300', strcat(name,'.png'));   % exort as png
-print('-depsc',strcat(name,'.eps'));    % export as eps
-print('-dpdf', '-painters',strcat(name,'.pdf'));    % export as pdf
+    if nargin <2
+        options =  'eps,png';
+    end
+
+
+    savefig(name);  % same as .fig
+
+    if not (isempty(strfind(options,'eps')))
+        print('-depsc',strcat(name,'.eps'));    % export as eps
+    end
+
+    if not (isempty(strfind(options,'pdf')))
+        print('-dpdf', '-painters',strcat(name,'.pdf'));    % export as pdf
+    end
+
+    if not (isempty(strfind(options,'png')))
+        print('-dpng','-r300', strcat(name,'.png'));   % exort as png
+    end
+
 
 end
