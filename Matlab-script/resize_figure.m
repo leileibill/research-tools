29fff5c46ce9fefe7f21f1de59e5d13c05652647
leileibill <leileibill@gmail.com>
2016-09-25 19:28:33 -0500

    Potential fix for white margin with subplots

--- Matlab-script/resize_figure.m
+++ Matlab-script/resize_figure.m
@@ -1,22 +1,28 @@
 function resize_figure(scale, ratio)
 
-if nargin <1
-    scale = 2;
-end
+	if nargin <1
+		scale = 2;
+	end
 
-if nargin < 2
-    ratio =0.618;
-end
+	if nargin < 2
+		ratio =0.618;
+	end
 
-width = 3.5*scale;
-height = 3.5*scale*ratio;
-% f = gcf;
-% f.Units = 'inches';   % works only after 2014b
+	width = 3.5*scale;
+	height = 3.5*scale*ratio;
+	% f = gcf;
+	% f.Units = 'inches';   % works only after 2014b
 
-po = 1;
-set(gcf, 'Units', 'inches')
-set(gcf, 'Position', [po po width height])
-set(gcf,'PaperPositionMode','auto')
-set(gcf,'PaperSize', [width height])
-set(gca,'LooseInset',get(gca,'TightInset')) % remove white margin
+	po = 1;
+	set(gcf, 'Units', 'inches')
+	set(gcf, 'Position', [po po width height])
+	set(gcf,'PaperPositionMode','auto')
+	set(gcf,'PaperSize', [width height])
+	
+    ha_all = findobj(gcf,'type','axes');    % find all axes in the current figure   
+    for index = 1:length(ha_all)
+		ha = ha_all(index);
+		set(ha,'LooseInset',get(ha,'TightInset')) % remove white margin
+	end
+
 end
