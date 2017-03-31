d5461ac68e0231b742922a9c47e56f52a9ddca40
leileibill <leileibill@gmail.com>
2017-03-31 11:31:31 -0500

    Added custom xcircuit library. Updated Matlab-script/read_ltspice_waveform.m to be more flexible. Now should be able to read waveform data with a single or multiple runs, with a single or multiple waveforms.

--- Matlab-script/read_ltspice_waveform_backup.m
+++ Matlab-script/read_ltspice_waveform_backup.m
@@ -3,13 +3,9 @@
 
 % Output is a structure array that has three field, info, time and value
 
-% Works for a waveform file with a single or multiple waveforms
-% Works for waveform file with a single or multiple simulation steps
-function y=read_ltspice_waveform(file_name,num_variable)
-
-    if nargin < 2
-        num_variable = 1;
-    end
+% Currently only works for a waveform file with a single waveform
+% Currently only works for waveform file with multiple simulation steps
+function y=read_ltspice_waveform(file_name)
     data_file=fopen(file_name);
     C=textscan(data_file,'%s',-1,'delimiter','\n');
 
@@ -18,36 +14,32 @@ function y=read_ltspice_waveform(file_name,num_variable)
     
     first_line = strsplit(lines{1});
     
-    y = struct('info',{},'time',{},'values',{});
+    y = struct('info',{},'time',{},'value',{});
 
-    i = 0;
+    i = 1;
     num_run = 0;
     while i < p     % go through each line
         i = i+1;
         display(i)
-        occurrance = strfind(lines{i},'time')
+        occurrance = strfind(lines{i},'Run');
         if isempty(occurrance) == 0    % the current line contains run information
             num_run = num_run + 1;
             y(num_run).info = lines(i);  % store the run info
             t = [];
             x = [];
             for j = (i+1):p   % now continue to read the data
-                occurrance = strfind(lines{j},'time');
+                occurrance = strfind(lines{j},'Run');
                 if isempty(occurrance) == 0    % stop if hit next run
                     i = j - 1;
                     break;
                 end
                 values = strsplit(lines{j});
                 t(end+1) = str2num(values{1});
-                temp = [];
-                for index = 1:num_variable
-                    temp(1,end+1) = str2num(values{index+1});
-                end
-                x(end+1,:) = temp;
+                x(end+1) = str2num(values{2});
                 i = p;
             end
             y(num_run).time = t;
-            y(num_run).values = x;
+            y(num_run).value = x;
         end
     end
     fclose(data_file);
