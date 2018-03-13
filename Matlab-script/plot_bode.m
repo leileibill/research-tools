c87057373d6a5e19d41985648d3c201390c06657
Yutian Lei <yulei@tesla.com>
2018-03-12 21:32:14 -0700

    Add line style as a parameter to the function

--- Matlab-script/plot_bode.m
+++ Matlab-script/plot_bode.m
@@ -1,17 +1,33 @@
-function [f, mag, phase] = plot_bode(tf, freq_min, freq_max)
-    [mag, phase, w] = bode(tf,{freq_min*2*pi freq_max*2*pi});
+function [f, mag, phase] = plot_bode(tf, freq_min, freq_max, line_style)
+    switch nargin
+        case 4
+            w = logspace(log10(freq_min), log10(freq_max), 100000) * 2 * pi;
+            [mag, phase] = bode(tf,w);
+        case 3
+            w = logspace(log10(freq_min), log10(freq_max), 100000) * 2 * pi;
+            [mag, phase] = bode(tf,w);
+            line_style = '-';
+        otherwise
+            [mag, phase, w] = bode(tf);
+            line_style = '-';
+    end
+
+
     f = w/2/pi;
     mag = mag(:);
     phase = phase(:);
 
-    subplot(2,1,1)
-    semilogx(f,20*log10(mag))
-    xlabel('Frequency (Hz)')
-    ylabel('Magnitude (dB)')
+    ax1 = subplot(2,1,1);
+    semilogx(f,20*log10(mag), line_style);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude (dB)');
     hold on;
-    subplot(2,1,2)
-    semilogx(f,phase)
-    xlabel('Frequency (Hz)')
-    ylabel('Phase (degree)')
+    ax2 = subplot(2,1,2);
+    semilogx(f,phase, line_style);
+    xlabel('Frequency (Hz)');
+    ylabel('Phase (degree)');
     hold on
+    
+    linkaxes([ax2,ax1],'x'); 
+    xlim([freq_min, freq_max]);
 end
