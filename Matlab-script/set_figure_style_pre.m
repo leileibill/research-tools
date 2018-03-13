642510c431582f60e1fc5100fc7119dbcd97246b
Yutian Lei <yulei@tesla.com>
2018-03-12 21:33:50 -0700

    Use line size, marker size and font size as function parameter

--- Matlab-script/set_figure_style_pre.m
+++ Matlab-script/set_figure_style_pre.m
@@ -1,12 +1,14 @@
-function set_figure_style_pre(scale)
+function set_figure_style_pre(line_scale, marker_scale, font_scale)
 
     if nargin <1
-        scale = 2;
+        line_scale = 2;
+        marker_scale = 2;
+        font_scale = 2;
     end
     
     % Line width
-    set(0,'DefaultLinelineWidth', 0.8*scale)
-    set(0,'DefaultLineMarkerSize',4*scale)
-    set(0,'DefaultAxesFontSize',8*scale)
+    set(0,'DefaultLinelineWidth', 0.8*line_scale)
+    set(0,'DefaultLineMarkerSize',4*marker_scale)
+    set(0,'DefaultAxesFontSize',8*font_scale)
 end
 
